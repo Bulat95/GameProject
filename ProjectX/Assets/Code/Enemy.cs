7e640ca3d0389041eab7f38b4b7cf74fb266803f
t95666f <t95666f@gmail.com>
2021-05-18 20:28:32 +0400

    Сделал преследование врагов

--- ProjectX/Assets/Code/Enemy.cs
+++ ProjectX/Assets/Code/Enemy.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : GameCharacter
 {
+    private NavMeshAgent agent;
     public override int Health { get; set; }
 
     public override void MovementLogic()
     {
         
+    }
+    private void Update()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(GameObject.Find("Player").transform.position);
     }
 }
